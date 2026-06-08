a4e84c4ee9d68995010a03aae3d32e54f7e898e4
Voryalis <etopro100mercy@gmail.com>
2026-06-08 10:52:45 +0400

    math palette

--- src/components/MathPalette.tsx
+++ src/components/MathPalette.tsx
@@ -25,12 +25,15 @@ function MathPalette({
         title="Math keyboard"
       >
         <svg viewBox="0 0 24 24" aria-hidden="true">
-          <rect x="3" y="5" width="18" height="14" rx="2" />
-          <path d="M7 9h.01" />
-          <path d="M11 9h.01" />
-          <path d="M15 9h.01" />
-          <path d="M17 13h.01" />
-          <path d="M7 13h6" />
+          <path d="M10 8h.01" />
+          <path d="M12 12h.01" />
+          <path d="M14 8h.01" />
+          <path d="M16 12h.01" />
+          <path d="M18 8h.01" />
+          <path d="M6 8h.01" />
+          <path d="M7 16h10" />
+          <path d="M8 12h.01" />
+          <rect width="20" height="16" x="2" y="4" rx="2" />
         </svg>
       </button>
 
